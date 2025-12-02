29760931f9d2ad4784034213aa467c7ad79812ce
sioxty <maksymslushayev@gmail.com>
2025-12-02 18:31:38 +0000

    ref

--- src/main.py
+++ src/main.py
@@ -1,23 +1,18 @@
 import logging
 
 from fastapi import FastAPI
+
 from src.utils import load_routers
-
-
-from src.users.router import router as users_router
-from src.auth.router import router as auth_router 
-
 
 log = logging.getLogger(__name__)
 
 app = FastAPI(title="Booking System API")
 
 
-
-load_routers(app) 
+load_routers(app)
 
 
 @app.get("/")
 async def root():
     """Перевірка стану додатку."""
-    return {"ok": True }
+    return {"ok": True}

aba6797fe8e2fa404ab5c126ac7ab34a3ffa0fef
sioxty <maksymslushayev@gmail.com>
2025-12-05 07:59:55 +0000

    add cors

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.utils import load_routers
 
 log = logging.getLogger(__name__)
 
-app = FastAPI(title="Booking System API")
+app = FastAPI(title="Ticket System API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500"],  # фронтенд
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 load_routers(app)
 
